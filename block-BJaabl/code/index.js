ddee9ec61261da403b8ed12c4a457e57d4f2786f
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2021-08-11 20:07:34 +0530

    update

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -27,7 +27,16 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
-
+function addTwoNumbers(firstNum , secondNum){
+  if(typeof firstNum!=='number'|| typeof secondNum!=='number'){
+    alert(`Enter Valid Input`);
+  }
+  else{
+    return console.log(firstNum+secondNum);
+  }
+  
+}
+addTwoNumbers(10,'12');
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
 can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
@@ -39,7 +48,25 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
-
+function calc(numA,numB,operation){
+  if(typeof numA!=='number'|| typeof numB!=='number'){
+    alert(`Enter a valid Input`);
+  }
+  else{
+    switch(operation){
+      case 'add':
+        return numA+numB;
+      case 'sub':
+        return numA-numB;
+      case 'mul':
+        return numA*numB;
+      case 'div':
+        return numA/numB;
+      default:
+        alert('enter valid operation');
+    }
+  }
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -49,7 +76,22 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear(year){
+  if(year%400===0) return true;
+  if(year%100===0) return false;
+  return year%4===0;
+}
+
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+
+function getFactorial(factNum){
+  let factNum=+prompt(`Enter a number`);
+  let final=1;
+  for(let i=factNum;i>=1;i--){
+    final*=i;
+  }
+  return final;
+}
